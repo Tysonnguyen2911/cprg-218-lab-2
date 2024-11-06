e9003511e964a90bd0ca00003f1dd38976a0a616
Tysonnguyen2911 <baon9976@gmail.com>
2024-11-06 16:57:54 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+sheep.addEventListener('click', function () {
+    console.log('sheep');
+    document.getElementById('animals').src = 'sheep.webp';
+}
+)
+beer.addEventListener('click', function () {
+    console.log('beer');
+    document.getElementById('animals').src = 'bear.webp';
+}
+)
+moose.addEventListener('click', function () {
+	console.log('moose');
+    document.getElementById('animals').src = 'moose.webp';
+}
+)
+wolf.addEventListener('click', function () {
+	console.log('wolf');
+    document.getElementById('animals').src = 'wolf.webp';
+}
+)
+elk.addEventListener('click', function () {
+	console.log('elk');
+    document.getElementById('animals').src = 'elk.webp';
+}
+)
